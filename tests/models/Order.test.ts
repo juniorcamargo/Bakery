d1b10879778cc89e0de138ebf0ac2391780c8135
Vinicius Boscoa <virb30@gmail.com>
2023-10-07 09:26:11 -0300

    tests(order): add should create order with invalid dish test

--- tests/models/Order.test.ts
+++ tests/models/Order.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test } from 'vitest';
 
+import { ErrorDishe } from '@/models/ErrorDishe';
 import { Coffee } from '@/models/morning/dishes/Coffee';
 import { Eggs } from '@/models/morning/dishes/Eggs';
 import { Toast } from '@/models/morning/dishes/Toast';
@@ -20,6 +21,22 @@ describe('Order tests', () => {
     };
 
     expect(order.getMeal().dishes).toHaveLength(3);
+    expect(order.getMeal()).toStrictEqual(expected);
+  });
+
+  test('should create an order with invalid dish', () => {
+    const order = new Order('1');
+    order.addDish(new Eggs());
+    order.addDish(new ErrorDishe());
+    order.addDish(new Coffee());
+    order.addDish(new Coffee());
+
+    const expected = {
+      error: true,
+      dishes: [new OrderDish(new Eggs(), 1)]
+    };
+
+    expect(order.getMeal().dishes).toHaveLength(1);
     expect(order.getMeal()).toStrictEqual(expected);
   });
 });
