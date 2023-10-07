a00f5f80446cc69598720a95950ff72ca4c62635
Vinicius Boscoa <virb30@gmail.com>
2023-10-07 10:15:02 -0300

    feat(receipt): add error print to receipt service

--- src/services/ReceiptService.ts
+++ src/services/ReceiptService.ts
@@ -4,8 +4,14 @@ export class ReceiptService {
   public static print(order: Order) {
     const dishes = order.getMeal();
 
-    return dishes
-      .map(orderDish => `${orderDish.dish.name}${orderDish.getAmount() > 1 ? `(x${orderDish.getAmount()})` : ''}`)
-      .join(', ');
+    const receiptItems = dishes.map(
+      orderDish => `${orderDish.dish.name}${orderDish.getAmount() > 1 ? `(x${orderDish.getAmount()})` : ''}`
+    );
+
+    if (order.hasError()) {
+      receiptItems.push('error');
+    }
+
+    return receiptItems.join(', ');
   }
 }
