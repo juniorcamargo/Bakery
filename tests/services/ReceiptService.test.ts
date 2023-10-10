73de0ccab26b03e37b895ed1b306a63eed332e4a
Vinicius Boscoa <virb30@gmail.com>
2023-10-09 22:55:11 -0300

    tests(receipt): adjust receipt input

--- tests/services/ReceiptService.test.ts
+++ tests/services/ReceiptService.test.ts
@@ -1,27 +1,28 @@
 import { describe, expect, test } from 'vitest';
 
-import { Coffee } from '@/models/morning/dishes/Coffee';
-import { Eggs } from '@/models/morning/dishes/Eggs';
-import { Toast } from '@/models/morning/dishes/Toast';
-import { Order } from '@/models/Order';
 import { ReceiptService } from '@/services/ReceiptService';
 
 describe('Receipt tests', () => {
   test('should print order receipt', () => {
-    const order = new Order('1');
-    order.addDish(new Eggs());
-    order.addDish(new Toast());
-    order.addDish(new Coffee());
-    order.addDish(new Coffee());
-
-    expect(ReceiptService.print(order)).toBe('eggs, toast, coffee(x2)');
+    const input = {
+      dishes: [
+        { name: 'eggs', amount: 1 },
+        { name: 'toast', amount: 1 },
+        { name: 'coffee', amount: 2 }
+      ],
+      error: false
+    };
+    expect(ReceiptService.print(input)).toBe('eggs, toast, coffee(x2)');
   });
 
   test('should print order receipt with error', () => {
-    const order = new Order('1');
-    order.addDish(new Eggs());
-    order.addDish(new Toast());
-
-    expect(ReceiptService.print(order, true)).toBe('eggs, toast, error');
+    const input = {
+      dishes: [
+        { name: 'eggs', amount: 1 },
+        { name: 'toast', amount: 1 }
+      ],
+      error: true
+    };
+    expect(ReceiptService.print(input)).toBe('eggs, toast, error');
   });
 });
